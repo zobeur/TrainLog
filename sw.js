2bf7cf1a83605007401335d136aef2b0f2ab6f41
zobeur <noe.fillaudeau@gmail.com>
2026-03-15 13:09:23 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'trainlog-v1';
+const FILES = ['/', '/index.html', '/manifest.json', '/icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/index.html')))
+  );
+});
